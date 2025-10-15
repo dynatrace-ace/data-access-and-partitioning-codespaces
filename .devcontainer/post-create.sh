4f9d77e4bc83b7de0d332bdfac79fd68947eaf1f
dynatrace <ace@dynatrace.com>
2025-10-15 22:03:56 +0000

    keeping it in post-create

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+# validate inputs
+set -euo pipefail
+
+echo "❌ Pre-validation failed intentionally."
+echo "This is a test to see how Codespaces handles initializeCommand failure."
+exit 1
+
 #loading functions to script
 export SECONDS=0
 source .devcontainer/util/source_framework.sh
